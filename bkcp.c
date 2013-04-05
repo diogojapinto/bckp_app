5d747ed1bf40b5837a486088bd9d8fdabc46159c
wso277 <wily1993@gmail.com>
2013-04-05 16:34:53 +0100

    teste

--- bkcp.c
+++ bkcp.c
@@ -1,1 +1,7 @@
-kdkdkdk
+sdkjbvsdkjbsdjvbsdlvjksdb vçkjsd vsçdkjv sd
+sdvsdkjvbsdçkjvbsdjks
+vsdkjvsdjvnsdçlknvsd
+sdvçksdjvnbsdçljvbweçi vsdjmk sdçkj sd
+sd nsdçkvjbsdçvj sdºljv bsdçjvbsdkn sdlk sd
+ sdnçsdkjvbsdçlj sdlj sdºklsdºoihjnsd
+vsdkdsj k sdçkj sdçkjndçkj sd jksdlksn sdjk sdj nsd
