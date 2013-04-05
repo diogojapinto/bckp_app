9b6c3bf104ef73a4dbcf68ef278205d7a9ea847f
wso277 <wily1993@gmail.com>
2013-04-05 17:18:15 +0100

    project started

--- bkcp.c
+++ bkcp.c
@@ -1,4 +1,15 @@
-kieiieieiei
-dslgweoh
-dslcbonvcob
-eiwetoewh\
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
+
+#define MAXBUFFER 50 
+
+int main(int argc, char *argv[]) {
+  
+  struct stat fileToCopy;
+  struct dirent fileOnFolder;
+  char *path;
+  
+  path = argv[1];
+  
+}
