7de04a398bb46c313c4273359e88b6275622a69d
wso277 <wily1993@gmail.com>
2013-04-20 00:21:10 +0100

    inicio do fillstructures e mudancas do makefile feitas de tarde

--- Makefile
+++ Makefile
@@ -3,23 +3,16 @@ CC = gcc
 CFLAGS = -Wall
 XHDRS = headers.h
 
-bckp: bckp.o common.o rstr
+all: bckp rstr
+
+bckp: bckp.o common.o
 	$(CC) $(CFLAGS) bckp.o common.o -o $@
 
 rstr: rstr.o common.o
 	$(CC) $(CFLAGS) rstr.o common.o -o $@
 
-#common.o: common.c common.h headers.h
-#	$(CC) $(CFLAGS) -c common.c
-
-#bckp.o: bckp.c bckp.h headers.h
-#	$(CC) $(CFLAGS) -c bckp.c
-
-#rstr.o: rstr.c rstr.h headers.h
-#	$(CC) $(CFLAGS) -c rstr.c
-
 %.o: %.c %.h $(XHDRS)
 	$(CC) $(CFLAGS) -c $<
 
 clean:
-	rm -f *.o *~
+	rm -f *.o
--- rstr.c
+++ rstr.c
@@ -1,3 +1,12 @@
+/**
+ * Esta me a dar segfault no files on folder, dentro do loadline. Ve o printf que fiz no load line, se fizer com um 
+ * numero aquilo manda um monte deles, se fizer printf do char lido ele da logo segfault
+ * ve a estrutura daquilo, eu penso que assim carrega as informcaoes necessarias para as 3, as outras duas ja
+ * testado e estava bem, so nao confirmei a filesonfolder por causa do segfault
+ * 
+ */
+
+
 #include "common.h"
 #include "rstr.h"
 #include "headers.h"
@@ -27,7 +36,7 @@ char **existing_files;
 int main(int argc, char **argv) {
   // sets the umask, to make shure it is possible to create the files as the
   // current user
-  //setbuf(stdout, NULL);
+  setbuf(stdout, NULL);
   umask(~(S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP |S_IROTH));
   
   /**
@@ -110,25 +119,121 @@ int main(int argc, char **argv) {
   /**
    * end of arguments verification
    */
+
+  fillStructures();
   
   return 0;
 }
+void fillExistingFiles(char *path) {
+  
+  DIR *bckp_dir;
+  struct dirent *bckp = NULL;
+  existing_files = malloc(sizeof(char*) * MAX_NR_FOLDERS);
+  
+  bckp_dir = opendir(path);
+  
+  while ((bckp = readdir(bckp_dir)) != NULL) {	//le o directorio com os backups
+    
+    struct stat st_bckp;
+    
+    // prepares the pathnames
+    char tmp_b[PATH_MAX];
+    strcpy(tmp_b, path);
+    strcat(tmp_b,"/");
+    strcat(tmp_b,bckp->d_name);
+    
+    // verifies if the  file is regular
+    if (lstat(tmp_b, &st_bckp) == -1) {
+      perror("lstat()");
+      exit(-1);
+    }
+    if (!S_ISREG(st_bckp.st_mode)) {
+      continue;
+    }
+    
+    int j = 0;
+    int found = 0;
+    char base[PATH_MAX];
+    char base1[PATH_MAX];
+    for (j=0; existing_files[j] != NULL; j++) {
+      strcpy(base1, basename(tmp_b));		//procura o nome no existing_files, se nao encontrar adiciona ao existing_files
+      strcpy(base, basename(existing_files[j]));
+      if ((strcmp(base,base1) == 0) || (strcmp(base1,"__bckpinfo__") == 0)){
+	printf("%s\n", base1);
+	found = -1;
+	break;
+      }
+    }
+    if (found == 0) {
+      existing_files[j] = malloc(sizeof(char) * PATH_MAX);
+      strcpy(existing_files[j], tmp_b);
+      printf("\n%s\n",existing_files[j]);
+    }
+  }
+}
 
+void fillFilesOnFolder(char *path, int i) {
+  
+  char line[PATH_MAX];
+  int fd=0;
+  int j=0;
+  if ((fd= open(path, O_RDONLY)) == -1) {
+    perror("open()");
+    exit(-1);
+  }
+  
+  printf("1\n");
+  files_on_folder[i] = malloc(sizeof(char*) * MAX_NR_FOLDERS);
+  
+  printf("2\n");
+  
+  while (loadLine(fd, line) != -1) {
+    printf("4\n");    
+    if (strlen(line) != 1) {
+      char tag[17];
+      char file_name[PATH_MAX];
+      sscanf(line, "%s %s", tag, file_name);
+      if (strcmp(tag, "<name>") == 0) {
+	printf("3\n");
+	files_on_folder[i][j] = malloc(sizeof(char) * PATH_MAX);	//le a linha se for nome adiciona a ij
+	strcpy(files_on_folder[i][j], file_name);
+	j++;
+      }
+    }
+  }
+}
 
 int fillStructures() {
+  
+  
+  loadDestDirectories();
+  int i = 0;
+  time_folder = malloc(sizeof(char*) * MAX_NR_FOLDERS);
+  files_on_folder = malloc(sizeof(char**) * MAX_NR_FOLDERS);
+  for(i=0; bckp_directories[i] != NULL; i++) {
+    
+    //fill time_folder
+    time_folder[i] = malloc(sizeof(char) * PATH_MAX);
+    strcpy(time_folder[i], basename(bckp_directories[i]));
+    
+    //fill files_on_folder
+    fillFilesOnFolder(bckp_directories[i],i);
+    
+    //fill existing_files
+    fillExistingFiles(bckp_directories[i]);
+  }
   return 0;
 }
 
-
-int verifyIfValidFolder(char *pathname) {
-  if (pathname == NULL) {
-    return 0;
+int verifyIfValidFolder(char *pathname) {			//move to common and add to load prev?
+if (pathname == NULL) {
+  return 0;
+} else {
+  int time[6];
+  if (sscanf(pathname, "%4d_%2d_%2d_%2d_%2d_%2d", &time[0], &time[1], &time[2], &time[3], &time[4], &time[5]) == 6) {
+    return -1;
   } else {
-    int time[6];
-    if (sscanf(pathname, "%4d_%2d_%2d_%2d_%2d_%2d", &time[0], &time[1], &time[2], &time[3], &time[4], &time[5]) == 6) {
-      return -1;
-    } else {
-      return 0;
-    }
+    return 0;
   }
+}
 }
